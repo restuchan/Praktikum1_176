12c0fcd3732acf74a43b8e19ed3efca51745e6a0
restuchan <restu.chan188@gmail.com>
2026-09-25 15:18:58 +0700

    changes value variable name from Android to Android saya in MainActivity.kt

--- app/src/main/java/com/chan/praktikum1/MainActivity.kt
+++ app/src/main/java/com/chan/praktikum1/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Praktikum1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android saya",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
